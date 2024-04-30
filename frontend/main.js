2ccdc4757b68cd189332791ad49cd86416f9f578
chabitblunk <cblunk097@gmail.com>
2024-04-29 22:41:26 -0600

    continue work on click events. being weird for some reason

--- frontend/main.js
+++ frontend/main.js
@@ -3,16 +3,15 @@ $(document).ready(() => {
   $(".div").not(".start").hide();
 });
 
-// show help
-$(".js-help").on("click", () => {
-  console.log("test");
-  $(".start").hide();
-  $(".help").show();
-});
-
-// show pishock config
-$(".js-pishock-config").on("click", () => {
-  console.log("test");
-  $(".start").hide();
-  $(".pishock-config").show();
+// show help or pishock config
+$(".js-help").on("click", "p", () => {
+  if (this.className.contains("js-help")) {
+    console.log($(this).text());
+    $(".start").hide();
+    $(".help").show();
+  } else {
+    console.log($(this).text());
+    $(".start").hide();
+    $(".pishock-config").show();
+  }
 });
