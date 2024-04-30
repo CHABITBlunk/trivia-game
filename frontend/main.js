9dbfc9b87b636214bf187b10c39a087931f9203f
chabitblunk <cblunk097@gmail.com>
2024-04-29 21:06:35 -0600

    working on getting help and config click events working

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,18 @@
 // start of game, only render start
-$(document).ready(() -> {
-  $(".start").not().hide();
+$(document).ready(() => {
+  $(".div").not(".start").hide();
 });
 
 // show help
-$(".js-help").on("click", () -> {
+$(".js-help").on("click", () => {
+  console.log("test");
   $(".start").hide();
   $(".help").show();
 });
 
 // show pishock config
-$(".js-pishock-config").on("click", () -> {
+$(".js-pishock-config").on("click", () => {
+  console.log("test");
   $(".start").hide();
   $(".pishock-config").show();
 });
