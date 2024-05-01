e252a427e0db90cf36a19d5c003862656cb19c69
chabitblunk <cblunk097@gmail.com>
2024-04-30 21:55:40 -0600

    commit so that nick and i have the same thing

--- frontend/main.js
+++ frontend/main.js
@@ -23,7 +23,7 @@ $(document).ready(() => {
   // send pishock config to backend after clicking save button
   $(".js-save").on("click", () => {
     axios
-      .post("/user/pishock-config", {
+      .post("/pishock-config", {
         operation: $(".pishock-config-operation").val(),
         duration: $(".pishock-config-duration").val(),
         intensity: $(".pishock-config-intensity").val(),
@@ -42,13 +42,53 @@ $(document).ready(() => {
     $(".game").show();
     // axios request for a question
     axios
-      .get("/game/question")
-      .then((response) => {})
-      .catch((error) => {});
+      .get("/question")
+      .then((response) => {
+        $(".game__problem").append(
+          createQuestion(
+            response.data.question,
+            response.data.answers,
+            response.data.correct,
+            response.data.ended
+          ),
+        );
+      })
+      .catch((error) => {
+        $(".game__problem").append("<p>Error: problem did not load</p>");
+      });
+  });
+
+  // submit answer
+  $(".js-submit-answer").on("click", () => {
+    if ($(".game__problem__answer:checked").attr("id") == "correct") {
+      $("div").not(".correct-answer").hide();
+      $(".correct-answer").show();
+    } else {
+      $("div").not(".incorrect-answer").hide();
+      $(".incorrect-answer").show();
+      // make http request to shock user
+    }
   });
 });
 
+// fisher-yates shuffle
+const shuffle = (array) => {
+  let i = array.length;
+  while (i != 0) {
+    let ri = Math.floor(Math.random() * i);
+    i--;
+    [array[i], array[ri]] = [array[ri], array[i]];
+  }
+};
+
 // create the html for a problem, including question and answers
-const createQuestion = (question, answers, correctAnswer) => {
-  return `<p class="p game__problem__question">${question}</p>`;
+// HACK: gave default values for question
+const createQuestion= (question = "What is 2+2?", answers = {"5", "4", "3", "Unsure"}, correct = "4") => {
+  let output = `<p class="p game__problem__question">${question}</p>`;
+  shuffle(answers);
+  answers.map((answer) => {
+    output += `<input type="radio" id="${answer === correct ? "correct" : "incorrect"}" class="game__problem__answer" value="${answer}" />`;
+  });
+  output += `<button class="btn js-submit-answer">Submit answer</button>`;
+  return output;
 };
--- server/models.py
+++ server/models.py
@@ -1,19 +1,14 @@
 class Player:
     """
-
     Represents a player in the game and the assocaited PiShock Settings
-
     Variables:
-
         player_name (str): name of the player.
         player_score (int): current score of the player, initially 0.
         num_incorrect (int): number of  incorrect answers the player has, initally 0
         pi_shock_setting (tuple): PiShock settings (operation, duration, intensity).
         pi_shock_code (str): Unique code to identify the PiShock device for this player.
         intensity_scale (bool): determine if intensity scales with incorrect answers.
-
     """
-
     def __init__(self, name, pi_shock_code, pi_shock_setting=(0, 0, 0)):
         self.player_name = name
         self.player_score = 0
@@ -25,39 +20,27 @@ class Player:
     def update_score(self, points):
         """
         Updates the players score by point value
-
         Args:
             points(int): The number of points to add to the player's score
-
         """
         self.player_score += points
 
     def increment_incorrect(self):
-        """
-
-        Increment the count of the players incorrect answers by one
-
-        """
-
+        """Increment the count of the players incorrect answers by one"""
         self.num_incorrect += 1
 
     def update_pi_shock_settings(self, settings):
         """
-
         Change the PiShock setting for this player
-
         Args:
             setting(tuple): A tuple containing the new operation, duration, and intensity PiShock settings
-
         """
-
         self.pi_shock_settings = settings
 
 
 class PiShockConfig:
     """
     Configuration for the PiShock device API access.
-
     Variable:
         username (str): Username for PiShock API authentication.
         apikey (str): API key for PiShock API authentication.
@@ -65,9 +48,7 @@ class PiShockConfig:
         name (str): Optional name for the script or application using the API, default 'TG_Bot_Script'.
         base_url (str): Base URL for the PiShock API operations.
         headers (dict): Headers to be used for API requests, defaulting to JSON content type.
-
     """
-
     def __init__(
         self,
         username,
